3063638c66d843ff9387b7866a019a56c53a3011
fzabid05 <abidzora88@gmail.com>
2019-11-04 14:35:16 +0100

    add_projet_one

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>ProjetGroup</title>
+    </head>
+    <body>
+        <h1 class="display-4 text-center text-info">Projet 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nihil accusamus sequi et. Expedita dignissimos autem natus, labore quibusdam dolorum deserunt aspernatur provident hic tempora nemo inventore corporis corrupti veniam nisi sunt eaque sit fugit consectetur commodi numquam. Doloribus modi illum facilis cumque velit voluptatum, sequi quos vitae? Possimus architecto rem atque sequi? Temporibus praesentium, nulla quis odit totam explicabo ducimus commodi sequi eius ratione veniam voluptatum amet? Sunt totam corporis vel similique dolores temporibus eius quis debitis voluptate quasi autem inventore ipsum expedita hic quas nobis explicabo rem possimus exercitationem dignissimos dolore perferendis, soluta mollitia. Consequatur maxime illo optio quod, hic tenetur fuga, incidunt in iste earum quibusdam maiores odit, ex laborum vitae saepe similique non est corrupti rerum? Dolores deserunt recusandae mollitia voluptatibus iste ex a, veniam nostrum ab quaerat nihil sit dolor cumque debitis, fuga molestias expedita rerum. Laudantium, voluptatum deleniti dolore corporis alias deserunt, suscipit illo cupiditate esse iste distinctio eos quos officiis animi sint excepturi ab doloribus? Ut nemo non illo delectus facilis quaerat cum, a fugiat beatae ipsum hic placeat, maiores culpa laboriosam dolorum. Similique officiis, dolorem voluptates corporis delectus ipsum dolorum iste quae, magnam dolore nobis iusto vitae, facilis molestiae accusamus placeat non doloribus maxime doloremque. Et provident nobis, vitae veniam excepturi illo enim ullam! Eos quidem illo minus fuga officiis beatae aspernatur, ab consequuntur assumenda ipsam provident, excepturi quia magni sunt laborum esse velit, consectetur reiciendis omnis vero nostrum aperiam rem. Beatae natus id error illum dolores delectus neque itaque aliquam quaerat! Debitis, laboriosam? Itaque possimus deserunt assumenda odio recusandae! Libero officia facere quam nobis consectetur atque beatae illo ipsa veniam adipisci quaerat natus dignissimos praesentium illum earum incidunt reiciendis voluptates id explicabo, ex eveniet dolorem rerum? Impedit voluptatem quos inventore dolorum ipsum quae, quaerat magni praesentium obcaecati saepe perferendis error sapiente!</p>
+        
+    </body>
+</html>
